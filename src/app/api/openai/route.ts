2f01625a7e7a0748d92916a056be75e265c28966
Yusef <you@example.com>
2024-02-12 20:15:35 +0100

    feat: added context to the bot

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -9,9 +9,15 @@ export async function POST(request: Request) {
   const { userText } = await request.json();
   const chatCompletion: OpenAI.Chat.ChatCompletion =
     await openai.chat.completions.create({
-      messages: [{ role: "user", content: userText }],
+      messages: [
+        {
+          role: "user",
+          content: `Tienes que responder como si fueras un bot en una página web, creada por Yusef, el cual es un desarrollador front-end, muy guapo ademas, el usuario ha puesto esto, responde en el idoma de su mensaje: ${userText}`,
+        },
+      ],
       model: "gpt-4",
     });
+  console.log(chatCompletion.choices[0].message.content);
   return NextResponse.json(
     { message: `${chatCompletion.choices[0].message.content}` },
     { status: 200 }
